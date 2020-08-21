5ba97954f2380753a6b9d616645fa1477630f396
Pablo Hazan <phazan@santandertecnologia.com.ar>
2020-08-21 18:18:37 -0300

    cambiar puerto y procesar pagos

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
-var express = require('express');
-var exphbs = require('express-handlebars');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mp = require('./mp');
 
-var app = express();
+const app = express();
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+app.get('/detail', async (req, res) => {
+    try {
+        const initPoint = await mp.getInitPoint(req.query);
+        res.render('detail', { ...req.query, initPoint });
+    } catch (error) {
+        res.redirect('/')
+    }
 });
+
+app.post('/notification', (req, res) => {
+    console.log(req.body)
+    res.status(200).send();
+})
+
+app.get('/success', (req, res) => {
+    res.render('success', req.query)
+});
+
+app.get('/pending', (req, res) => {
+    const message = 'Pago pendiente';
+    res.render('notSuccess', { message });
+});
+
+app.get('/failure', (req, res) => {
+    const message = 'No se pudo realizar el pago';
+    res.render('notSuccess', { message })
+});
+
 
 app.use(express.static('assets'));
 
 app.use('/assets', express.static(__dirname + '/assets'));
 
-app.listen(3000);
+app.listen(8080);
---
+++ mp.js
@@ -0,0 +1,79 @@
+const mercadopago = require('mercadopago');
+
+mercadopago.configure({
+    access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
+    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
+});
+
+const baseUrl = 'https://pablohazan-mp-ecommerce-nodejs.herokuapp.com/'
+
+const createItem = (title, unit_price, quantity, picture_url) => ({
+    id: '1234',
+    title,
+    unit_price,
+    quantity,
+    category_id: 'phones',
+    description: 'Dispositivo móvile de Tienda e-commerce',
+    picture_url
+})
+
+const external_reference = 'phazan.fiuba@gmail.com';
+const payer = {
+    name: 'Lalo',
+    surname: 'Landa',
+    email: 'test_user_63274575@testuser.com',
+    phone: {
+        area_code: '11',
+        number: 22223333,
+    },
+    address: {
+        zip_code: '1111',
+        street_name: 'False',
+        street_number: 123
+    },
+    identification: {
+        type: "",
+        number: "471923173"
+    },
+}
+
+const auto_return = 'approved';
+const notification_url = `${baseUrl}notification`;
+const back_urls = {
+    success: `${baseUrl}success`,
+    pending: `${baseUrl}pending`,
+    failure: `${baseUrl}failure`,
+};
+
+const payment_methods = {
+    installments: 6,
+    default_installments: 1,
+    excluded_payment_methods: [{ id: 'amex' }],
+    excluded_payment_types: [{ id: 'atm' }],
+};
+
+
+const createPreference = (title, price, unit, img) => ({
+    items: [createItem(title, parseFloat(price), parseInt(unit), img)],
+    external_reference,
+    payer,
+    back_urls,
+    payment_methods,
+    notification_url,
+    auto_return
+})
+
+const getInitPoint = async ({ title, price, unit, img }) => {
+    const preference = createPreference(title, price, unit, img);
+    try {
+        const response = await mercadopago.preferences.create(preference)
+        return response.body.init_point
+    } catch (error) {
+        console.log(error);
+        throw errror
+    }
+}
+
+module.exports = {
+    getInitPoint
+}
